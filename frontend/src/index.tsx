a4bcec9f24ae40583998da28405e584b32abba47
techyNonso <williamikeji@gmail.com>
2023-05-04 14:04:55 +0100

    wallet connect added

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ModalProvider} from '@particle-network/connect-react-ui';
+import { WalletEntryPosition } from '@particle-network/auth';
+import { evmWallets } from "@particle-network/connect";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ModalProvider
+      options={{
+        projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+        clientKey: `${process.env.REACT_APP_CLIENT_KEY}`,
+        appId: `${process.env.REACT_APP_APP_ID}`,
+        chains: [
+          { id: 97, name: "Bsc" },
+          { id: 1, name: "Ethereum" },
+        ],
+        particleWalletEntry: {
+          //optional: particle wallet config
+          displayWalletEntry: true, //display wallet button when connect particle success.
+          defaultWalletEntryPosition: WalletEntryPosition.BR,
+          supportChains: [
+            { id: 97, name: "Bsc" },
+            { id: 1, name: "Ethereum" },
+          ],
+          customStyle: {}, //optional: custom wallet style
+        }, 
+        wallets: evmWallets({ qrcode: false }),
+      }}
+      theme={"dark"}
+      language={"en"} //optional：localize, default en
+      walletSort={["Particle Auth", "Wallet"]} //optional：walelt order
+      particleAuthSort={[
+        "email",
+        "google",
+        "apple",
+        "facebook",
+      ]}
+    >
+
+      <App />
+      </ModalProvider>
   </React.StrictMode>
 );
 
