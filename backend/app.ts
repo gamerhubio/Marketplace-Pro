d87eedf9d5de037be36290f7a97e1b892e34f42c
Chukwunonso Ikeji <williamikeji@gmail.com>
2024-08-08 20:29:35 +0100

    revert

--- backend/app.ts
+++ backend/app.ts
@@ -24,7 +24,11 @@ import cors from "cors";
 // middleware
 app.use(
   cors({
-    origin: ["*"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://www.gamer-hub.io",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
