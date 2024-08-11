d39cf1b8b6ea41a6ff07d4a9263d0db1a331c3c4
Chukwunonso Ikeji <williamikeji@gmail.com>
2024-08-11 20:22:46 +0100

    done

--- backend/app.ts
+++ backend/app.ts
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://www.gamer-hub.io",
+    "https://gamer-hub.io",
   ];
 
   const origin = req.headers.origin;
