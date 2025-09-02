2437dbff00cbdca80a2796a4b1e652e779fb30ad
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2025-09-02 16:31:51 +0530

    feat: roadmap changes

--- frontend/src/modules/landing/data.ts
+++ frontend/src/modules/landing/data.ts
@@ -1,201 +1,213 @@
 export const gamerhubSlider = [
-  {
-    img: "/images/landing/gamerhub-slider/slider1.png",
-    width: 765,
-  },
-  {
-    img: "/images/landing/gamerhub-slider/slider1.png",
-    width: 765,
-  },
-  {
-    img: "/images/landing/gamerhub-slider/slider1.png",
-    width: 765,
-  },
-  {
-    img: "/images/landing/gamerhub-slider/slider1.png",
-    width: 765,
-  },
+    {
+        img: "/images/landing/gamerhub-slider/slider1.png",
+        width: 765,
+    },
+    {
+        img: "/images/landing/gamerhub-slider/slider1.png",
+        width: 765,
+    },
+    {
+        img: "/images/landing/gamerhub-slider/slider1.png",
+        width: 765,
+    },
+    {
+        img: "/images/landing/gamerhub-slider/slider1.png",
+        width: 765,
+    },
 ];
 
 export const streamingSlider = [
-  {
-    img: "/images/landing/streaming-slider/streaming-img.png",
-    video: "",
-    link: "https://google.com",
-  },
-  {
-    img: "/images/landing/streaming-slider/streaming-img.png",
-    video: "",
-    link: "https://google.com",
-  },
-  {
-    img: "/images/landing/streaming-slider/streaming-img.png",
-    video: "",
-    link: "https://google.com",
-  },
-  {
-    img: "/images/landing/streaming-slider/streaming-img.png",
-    video: "",
-    link: "https://google.com",
-  },
+    {
+        img: "/images/landing/streaming-slider/streaming-img.png",
+        video: "",
+        link: "https://google.com",
+    },
+    {
+        img: "/images/landing/streaming-slider/streaming-img.png",
+        video: "",
+        link: "https://google.com",
+    },
+    {
+        img: "/images/landing/streaming-slider/streaming-img.png",
+        video: "",
+        link: "https://google.com",
+    },
+    {
+        img: "/images/landing/streaming-slider/streaming-img.png",
+        video: "",
+        link: "https://google.com",
+    },
 ];
 
 export const marketplaceSlider = [
-  "/images/landing/marketplace-slider/marketplace-img.png",
-  "/images/landing/marketplace-slider/marketplace-img.png",
-  "/images/landing/marketplace-slider/marketplace-img.png",
-  "/images/landing/marketplace-slider/marketplace-img.png",
+    "/images/landing/marketplace-slider/marketplace-img.png",
+    "/images/landing/marketplace-slider/marketplace-img.png",
+    "/images/landing/marketplace-slider/marketplace-img.png",
+    "/images/landing/marketplace-slider/marketplace-img.png",
 ];
 
 export const merchSlider = [
-  "/images/landing/merch-slider/merch-img.png",
-  "/images/landing/merch-slider/merch-img.png",
-  "/images/landing/merch-slider/merch-img.png",
-  "/images/landing/merch-slider/merch-img.png",
+    "/images/landing/merch-slider/merch-img.png",
+    "/images/landing/merch-slider/merch-img.png",
+    "/images/landing/merch-slider/merch-img.png",
+    "/images/landing/merch-slider/merch-img.png",
 ];
 
 export const tokenomicsData = [
-  {
-    title: "Seed Sale Price",
-    value: "1 GHT = $0.005",
-  },
-  {
-    title: "Private Sale Price",
-    value: "1 GHT = $0.01",
-  },
-  {
-    title: "Public Sale Price",
-    value: "1 GHT = $0.015",
-  },
-  {
-    title: "Total Supply",
-    value: "500, 000, 000",
-  },
+    {
+        title: "Seed Sale Price",
+        value: "1 GHTAI = $0.005",
+    },
+    {
+        title: "Private Sale Price",
+        value: "1 GHTAI = $0.01",
+    },
+    {
+        title: "Public Sale Price",
+        value: "1 GHTAI = $0.015",
+    },
+    {
+        title: "Total Supply",
+        value: "1, 000, 000, 000",
+    },
 ];
 
 export const tokenomicsCardData = [
-  {
-    img: "/images/landing/tokenomics/token1.png",
-    title: "Ecosystem Currency",
-    description:
-      "GHT will be the major gaming and trading currency on GameHub as well as the NFT & In-game asset marketplaces.",
-  },
-  {
-    img: "/images/landing/tokenomics/token2.png",
-    title: "GameFi and MetaFi",
-    description:
-      "GHT will be used for game token mining and other GameFi services.",
-  },
-  {
-    img: "/images/landing/tokenomics/token3.png",
-    title: "Ecosystem Governance",
-    description:
-      "GHT serves as the governance token for the GamerHub ecosystem.",
-  },
-  {
-    img: "/images/landing/tokenomics/token4.png",
-    title: "Exclusive Access",
-    description:
-      "GHT holders are granted access to exclusive games and streaming content, events and demos",
-  },
-  {
-    img: "/images/landing/tokenomics/token5.png",
-    title: "Merchandise",
-    description:
-      "GHT is the main currency for branding and trade of merchandise",
-  },
+    {
+        img: "/images/landing/tokenomics/token1.png",
+        title: "Ecosystem Currency",
+        description:
+            "GHTAI will be the major gaming and trading currency on GameHub as well as the NFT & In-game asset marketplaces.",
+    },
+    {
+        img: "/images/landing/tokenomics/token2.png",
+        title: "GameFi and MetaFi",
+        description:
+            "GHTAI will be used for game token mining and other GameFi services.",
+    },
+    {
+        img: "/images/landing/tokenomics/token3.png",
+        title: "Ecosystem Governance",
+        description:
+            "GHTAI serves as the governance token for the GamerHub ecosystem.",
+    },
+    {
+        img: "/images/landing/tokenomics/token4.png",
+        title: "Exclusive Access",
+        description:
+            "GHTAI holders are granted access to exclusive games and streaming content, events and demos",
+    },
+    {
+        img: "/images/landing/tokenomics/token5.png",
+        title: "Merchandise",
+        description:
+            "GHTAI is the main currency for branding and trade of merchandise",
+    },
 ];
 
 export const roadmapData = [
-  {
-    desc: ["Beta Testing", "Token Utility Design"],
-    year: "2023",
-    month: "Q4",
-  },
-  {
-    desc: ["Feature Enhancements", "MVP Beta Testing"],
-    year: "2024",
-    month: "Q1",
-  },
-  {
-    desc: ["Public Round Launch", "Token Listing", "Staking and Liquidity Lock"],
-    year: "2024",
-    month: "Q2",
-  },
-  {
-    desc: [
-      "Platform Integrations",
-      "Launch GamerHub Mobile App and PC Launcher",
-    ],
-    year: "2023",
-    month: "Q3",
-  },
-  {
-    desc: ["Launch in-game asset marketplace", "Launch Game Streaming"],
-    year: "2024",
-    month: "Q4",
-  },
-  {
-    desc: ["User acquisition campaigns", "Platform expansion"],
-    year: "2025",
-    month: "Q1",
-  },
+    {
+        desc: ["Demo & MVP Development", "Game & User Profile Integration"],
+        year: "2023",
+        month: "Q1 - Q4",
+    },
+    {
+        desc: [
+            "PC Launcher Release (V1 & V2)",
+            "In-game Asset Marketplace Launch",
+        ],
+        year: "2024",
+        month: "Q1 - Q4",
+    },
+    {
+        desc: [
+            "Gamer Reward System Expansion",
+            "DAO Framework Integration",
+            "Game Hackathons",
+        ],
+        year: "2025",
+        month: "Q1 - Q3",
+    },
+    {
+        desc: [
+            "Web2 to Web3 SDKs",
+            "Metaverse & Inter-metaverse Competitions",
+            "Cross-platform Interoperability",
+        ],
+        year: "2025",
+        month: "Q4",
+    },
+    {
+        desc: ["AI Game Companion", "GamerHub AI/VR Metaverse Development"],
+        year: "2026",
+        month: "Q1 - Q4",
+    },
+    {
+        desc: [
+            "Tokenization of AI Game Assets & Elements",
+            "SDK & API Development",
+            "ZK Tech & zkNFT Marketplace/Bridge",
+        ],
+        year: "2027",
+        month: "Q1",
+    },
 ];
 
 export const teamData = [
-  {
-    img: "/images/landing/team/victor.png",
-    name: "Victor Kaycee",
-    role: "Co-founder / CEO",
-    linkedin: "https://www.linkedin.com/in/victor-kaycee"
-  },
-  {
-    img: "/images/landing/team/glory.png",
-    name: "Glory Lucas",
-    role: "Co-founder / CMO",
-    linkedin: "https://linkedin.com/in/GloryLucas"
-  },
+    {
+        img: "/images/landing/team/victor.png",
+        name: "Victor Kaycee",
+        role: "Co-founder / CEO",
+        linkedin: "https://www.linkedin.com/in/victor-kaycee",
+    },
+    {
+        img: "/images/landing/team/glory.png",
+        name: "Glory Lucas",
+        role: "Co-founder / CMO",
+        linkedin: "https://linkedin.com/in/GloryLucas",
+    },
 
-  {
-    img: "/images/landing/team/caleb.png",
-    name: "Caleb Lucas",
-    role: "Engineering Manager",
-    linkedin: "https://www.linkedin.com/in/cmdlucas"
-  },
-  // {
-  //   img: "/images/landing/team/ken.png",
-  //   name: "Ken Suzuki",
-  //   role: "Core Contributor",
-  //   
-  // },
-  {
-    img: "/images/landing/team/jeffery.png",
-    name: "Jeffery Pan",
-    role: "Institutional Advisor from SoftBank",
-    linkedin: "https://www.linkedin.com/in/nasa-b64b53215/"
-  },
-  {
-    img: "/images/landing/team/pekka.png",
-    name: "Pekka Kelkka",
-    role: "Institutional Advisor",
-    linkedin: "https://www.linkedin.com/in/pekkakelkka/"
-  },
-  {
-    img: "/images/landing/team/sam.png",
-    name: "Sam Vukmirica",
-    role: "Gaming Advisor",
-    linkedin: "https://www.linkedin.com/in/simo-vukmirica-001/"
-  },
-  {
-    img: "/images/landing/team/robbie.png",
-    name: "Robbie Joe",
-    role: "Polygon Advisor / CEO Kommunitas",
-    linkedin: "https://www.linkedin.com/in/robby678/",
-  },
-  {
-    img: "/images/landing/team/anita.png",
-    name: "Anita Kalergis",
-    role: "Institutional Advisor",
-    linkedin: "https://www.linkedin.com/in/anitakalergis/",
-  },
+    {
+        img: "/images/landing/team/caleb.png",
+        name: "Caleb Lucas",
+        role: "Engineering Manager",
+        linkedin: "https://www.linkedin.com/in/cmdlucas",
+    },
+    // {
+    //   img: "/images/landing/team/ken.png",
+    //   name: "Ken Suzuki",
+    //   role: "Core Contributor",
+    //
+    // },
+    {
+        img: "/images/landing/team/jeffery.png",
+        name: "Jeffery Pan",
+        role: "Institutional Advisor from SoftBank",
+        linkedin: "https://www.linkedin.com/in/nasa-b64b53215/",
+    },
+    {
+        img: "/images/landing/team/pekka.png",
+        name: "Pekka Kelkka",
+        role: "Institutional Advisor",
+        linkedin: "https://www.linkedin.com/in/pekkakelkka/",
+    },
+    {
+        img: "/images/landing/team/sam.png",
+        name: "Sam Vukmirica",
+        role: "Gaming Advisor",
+        linkedin: "https://www.linkedin.com/in/simo-vukmirica-001/",
+    },
+    {
+        img: "/images/landing/team/robbie.png",
+        name: "Robbie Joe",
+        role: "Polygon Advisor / CEO Kommunitas",
+        linkedin: "https://www.linkedin.com/in/robby678/",
+    },
+    {
+        img: "/images/landing/team/anita.png",
+        name: "Anita Kalergis",
+        role: "Institutional Advisor",
+        linkedin: "https://www.linkedin.com/in/anitakalergis/",
+    },
 ];
--- frontend/src/modules/landing/token/index.tsx
+++ frontend/src/modules/landing/token/index.tsx
@@ -1,44 +1,47 @@
 import React from "react";
 import {
-  TokenButton,
-  TokenButtonGroup,
-  TokenSectionWrapper,
-  TokenomicsCardGroupWrapper,
-  TokenomicsItem,
-  TokenomicsWrapper,
+    TokenButton,
+    TokenButtonGroup,
+    TokenSectionWrapper,
+    TokenomicsCardGroupWrapper,
+    TokenomicsItem,
+    TokenomicsWrapper,
 } from "./styles";
 import { Button, TokenomicsCard } from "../../../components";
 import { tokenomicsCardData, tokenomicsData } from "../data";
 
 export const TokenSection: React.FC = () => {
-  return (
-    <TokenSectionWrapper id="tokenomics">
-      <h1 data-aos="fade-up">
-        <strong> GHT </strong> TOKEN <img src="/images/landing/ght-token.png" alt="" />
-      </h1>
-      <p data-aos="fade-up">
-        The GHT token is a Multichain token that will be created on blockchains like Solana, BSC, Ethereum, to give the whole gaming ecosystem an easy access to participate in the gaming revolution
-      </p>
-      <TokenButtonGroup data-aos="fade-up">
-        <Button width={231}>View Contract Address</Button>
-        {/* <TokenButton>
+    return (
+        <TokenSectionWrapper id="tokenomics">
+            <h1 data-aos="fade-up">
+                <strong> GHTAI </strong> TOKEN{" "}
+                <img src="/images/landing/ght-token.png" alt="" />
+            </h1>
+            <p data-aos="fade-up">
+                The GHTAI token is a Multichain token that will be created on
+                blockchains like Solana, BSC, Ethereum, to give the whole gaming
+                ecosystem an easy access to participate in the gaming revolution
+            </p>
+            <TokenButtonGroup data-aos="fade-up">
+                <Button width={231}>View Contract Address</Button>
+                {/* <TokenButton>
           <span>Copy Contract Address</span>
         </TokenButton> */}
-      </TokenButtonGroup>
-      <h3 data-aos="fade-up">TOKENOMICS</h3>
-      <TokenomicsWrapper>
-        {tokenomicsData.map((item, key) => (
-          <TokenomicsItem key={key}>
-            <h4>{item.title}</h4>
-            <span>{item.value}</span>
-          </TokenomicsItem>
-        ))}
-      </TokenomicsWrapper>
-      <TokenomicsCardGroupWrapper>
-        {tokenomicsCardData.map((item, key) => (
-          <TokenomicsCard key={key} {...item} />
-        ))}
-      </TokenomicsCardGroupWrapper>
-    </TokenSectionWrapper>
-  );
+            </TokenButtonGroup>
+            <h3 data-aos="fade-up">TOKENOMICS</h3>
+            <TokenomicsWrapper>
+                {tokenomicsData.map((item, key) => (
+                    <TokenomicsItem key={key}>
+                        <h4>{item.title}</h4>
+                        <span>{item.value}</span>
+                    </TokenomicsItem>
+                ))}
+            </TokenomicsWrapper>
+            <TokenomicsCardGroupWrapper>
+                {tokenomicsCardData.map((item, key) => (
+                    <TokenomicsCard key={key} {...item} />
+                ))}
+            </TokenomicsCardGroupWrapper>
+        </TokenSectionWrapper>
+    );
 };
